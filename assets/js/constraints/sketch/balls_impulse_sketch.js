91ef9edeace26e31e37bcfc2d4dc7c0d1c9124ef
Kozak Roman <kozakromch@yandex.ru>
2025-04-28 19:26:21 +0300

    try to fix path

--- assets/js/constraints/sketch/balls_impulse_sketch.js
+++ assets/js/constraints/sketch/balls_impulse_sketch.js
@@ -1,6 +1,6 @@
-import all_pairs_namespace from '../../../../js/collisions/broadphase/all_pairs.min.js';
-import main_visualizator_namespace from '../../../../js/common/main_vis.min.js';
-import p5 from '../../../../js/libs/p5.min.js';
+import all_pairs_namespace from '../../../js/collisions/broadphase/all_pairs.min.js';
+import main_visualizator_namespace from '../../../js/common/main_vis.min.js';
+import p5 from '../../../js/libs/p5.min.js';
 
 {
   let my_interface = new all_pairs_namespace.AllPairsInterface(
